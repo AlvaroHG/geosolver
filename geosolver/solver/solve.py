67938b33dc96b3b2a6b32d277a5dae2c1edcd6ed
Alvaro Herrasti <alvaroh@alvaroh.local>
2017-02-03 12:57:34 -0800

    Fixes to run and get results

--- geosolver/solver/solve.py
+++ geosolver/solver/solve.py
@@ -19,6 +19,7 @@ def solve(given_formulas, choice_formulas=None, assignment=None):
     #1. Find query formula in true formulas
     true_formulas = []
     query_formula = None
+    print("Given formulas: {0}".format(given_formulas))
     for formula in given_formulas:
         assert isinstance(formula, FormulaNode)
         if formula.has_signature("What") or formula.has_signature("Which") or formula.has_signature("Find"):
@@ -41,6 +42,8 @@ def solve(given_formulas, choice_formulas=None, assignment=None):
             for key, choice_formula in choice_formulas.iteritems():
                 equal_formula = FormulaNode(signatures['Equals'], [ns.assignment['What'], choice_formula])
                 out[key] = ns.evaluate(equal_formula)
+		print equal_formula
+		print ns.assignment["What"]
 
             """
             ns = NumericSolver(true_formulas)
@@ -67,6 +70,9 @@ def solve(given_formulas, choice_formulas=None, assignment=None):
             for key, choice_formula in choice_formulas.iteritems():
                 replaced_formula = FormulaNode(signatures['Equals'], [query_formula.children[0], choice_formula])
                 out[key] = ns.evaluate(replaced_formula)
+		out[key].formula = replaced_formula
+		print replaced_formula
+		print("Key: ", out[key])
         # display_entities(ns)
 
 
